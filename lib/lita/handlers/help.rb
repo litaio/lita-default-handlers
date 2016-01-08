b251cf2948b55dfea15f1990c6a105cba74be0b6
Tristan Chong <tristanchong@gmail.com>
2016-01-08 13:15:49 -0800

    Reduce help handler wall of text
    This change allows help topics to be filtered by handler and an optional
    substring.

--- lib/lita/handlers/help.rb
+++ lib/lita/handlers/help.rb
@@ -5,18 +5,27 @@ module Lita
     class Help
       extend Handler::ChatRouter
 
-      route(/^help\s*(.+)?/, :help, command: true, help: {
-        "help" => t("help.help_value"),
-        t("help.help_command_key") => t("help.help_command_value")
-      })
+      route(
+        /^help\s*((?<handler>\S+)\s*(?<substring>.+)?)?/i,
+        :help, command: true, help: {
+          "help" => t("help.help_value"),
+          t("help.help_handler_key") => t("help.help_handler_value"),
+          t("help.help_command_key") => t("help.help_command_value")
+        }
+      )
 
       # Outputs help information about Lita commands.
       # @param response [Response] The response object.
       # @return [void]
       def help(response)
-        output = build_help(response)
-        output = filter_help(output, response)
-        response.reply_privately output.join("\n")
+        handler = response.match_data["handler"]
+        return response.reply_privately(
+          t("help.info", address: address) + "\n" + list_handlers.join("\n")
+        ) if handler.nil?
+
+        substring = response.match_data["substring"]
+        output = list_commands(handler, substring, response.user)
+        response.reply_privately(output.join("\n"))
       end
 
       private
@@ -28,39 +37,49 @@ module Lita
         end
       end
 
-      # Creates an array of help info for all registered routes.
-      def build_help(response)
+      # Creates an alphabetically-sorted array containing the names of all
+      # installed handlers.
+      def list_handlers
         robot.handlers.map do |handler|
-          next unless handler.respond_to?(:routes)
+          handler.namespace if handler.respond_to?(:routes)
+        end.flatten.compact.sort
+      end
 
+      # Creates an array of help info for a specified handler. Optionally
+      # filters commands matching a given substring.
+      def list_commands(handler_name, substring, user)
+        handlers = robot.handlers.select { |handler| handler.namespace == handler_name.strip.downcase }
+        return ["No matching handlers found for '#{handler_name}'"] if handlers.empty?
+        output = handlers.map do |handler|
           handler.routes.map do |route|
             route.help.map do |command, description|
-              if authorized?(response.user, route.required_groups)
-                help_command(route, command, description)
-              end
+              string = help_command(route, command, description)
+              string << t("help.unauthorized") unless authorized?(user, route.required_groups)
+              string
             end
           end
-        end.flatten.compact
+        end.flatten
+        filter_help(output, substring)
       end
 
       # Filters the help output by an optional command.
-      def filter_help(output, response)
-        filter = response.matches[0][0]
-
-        if filter
-          output.select { |line| /(?:@?#{name}[:,]?)?#{filter}/i === line }
-        else
-          output
-        end
+      def filter_help(output, substring)
+        return output if substring.nil?
+        output.select { |line| /(?:@?#{address})?#{substring}/i === line }
       end
 
       # Formats an individual command's help message.
       def help_command(route, command, description)
-        command = "#{name}: #{command}" if route.command?
+        command = "#{address}#{command}" if route.command?
         "#{command} - #{description}"
       end
 
       # The way the bot should be addressed in order to trigger a command.
+      def address
+        robot.config.robot.alias || "#{name}: "
+      end
+
+      # Fallback in case no alias is defined.
       def name
         robot.config.robot.mention_name || robot.config.robot.name
       end
--- spec/lita/handlers/help_spec.rb
+++ spec/lita/handlers/help_spec.rb
@@ -5,25 +5,51 @@ describe Lita::Handlers::Help, lita_handler: true do
   it { is_expected.to route_command("help foo").to(:help) }
 
   describe "#help" do
-    let(:secret_handler_class) do
+    let(:dummy_handler_class) do
       Class.new(Lita::Handler) do
+        def self.name
+          "Dummy"
+        end
+
         route(/secret/, :secret, restrict_to: :the_nobodies, help: {
-          "secret" => "no one should ever see this help message"
+          "secret" => "This help message should be accompanied by a caveat"
         })
       end
     end
 
-    it "sends help information for all commands" do
-      send_command("help")
-      expect(replies.last).to match(
-        /#{robot.mention_name}: help.+#{robot.mention_name}: help COMMAND/m
+    before do
+      registry.register_handler(dummy_handler_class)
+      allow(Gem).to receive(:loaded_specs).and_return(
+        { 'lita-dummy' => double('Gem', description: 'A dummy handler.') }
       )
+      allow(robot.config.robot).to receive(:alias).and_return("!")
     end
 
-    it "sends help information for commands starting with COMMAND" do
-      send_command("help help COMMAND")
-      expect(replies.last).to match(/help COMMAND - Lists/)
-      expect(replies.last).not_to match(/help - Lists/)
+    it "lists all installed handlers in alphabetical order" do
+      send_command("help")
+      expect(replies.last).to match(/^Type '!help HANDLER'.+installed:\ndummy\nhelp$/)
+    end
+
+    it "sends help information for all commands under a given handler" do
+      send_command("help help")
+      expect(replies.last).to match(/!help(?:.+!help HANDLER){2}/m)
+    end
+
+    it "sends help information for commands matching a substring under a given handler" do
+      send_command("help help available")
+      expect(replies.last).to match(/!help HANDLER - Lists/)
+      expect(replies.last).not_to match(/help information/)
+    end
+
+    it "uses the mention name when no alias is defined" do
+      allow(robot.config.robot).to receive(:alias).and_return(nil)
+      send_command("help help")
+      expect(replies.last).to match(/#{robot.mention_name}: help/)
+    end
+
+    it "responds with an error if no matching handler is installed" do
+      send_command("help asdf")
+      expect(replies.last).to match(/^No matching handlers found for 'asdf'$/)
     end
 
     it "doesn't crash if a handler doesn't have routes" do
@@ -43,16 +69,16 @@ describe Lita::Handlers::Help, lita_handler: true do
         user
       end
 
-      before { registry.register_handler(secret_handler_class) }
-
-      it "doesn't show help for commands the user doesn't have access to" do
-        send_command("help")
-        expect(replies.last).not_to include("secret")
+      it "shows the unauthorized message for commands the user doesn't have access to" do
+        send_command("help dummy")
+        expect(replies.last).to include("secret")
+        expect(replies.last).to include("Unauthorized")
       end
 
-      it "shows help for restricted routes if the user has access" do
-        send_command("help", as: authorized_user)
+      it "omits the unauthorized message if the user has access" do
+        send_command("help dummy", as: authorized_user)
         expect(replies.last).to include("secret")
+        expect(replies.last).not_to include("Unauthorized")
       end
     end
   end
